337e618c363e013a092c0198f0d88e9e55ba0c89
s4nyx <sanyscott4139@gmail.com>
2026-01-12 08:28:13 +0800

    feat: add comment on run.py

--- run.py
+++ run.py
@@ -2,9 +2,13 @@ import gdal2tiles
 import os
 from multiprocessing import freeze_support
 
+#default tile size 256
+
 # Simple raster profile generation with depth 9
-input_file = 'finalmap_grid_output.tif'
-output_dir = 'output_tiles_test_final/'
+input_file = 'finalmap_grid_output.tif'  # Input raster file
+output_dir = 'output_tiles_test_final/'  # Output directory
+
+# Function to generate tiles
 
 def generate_tiles():
     if os.path.exists(input_file):
@@ -13,9 +17,9 @@ def generate_tiles():
         gdal2tiles.generate_tiles(
             input_file, 
             output_dir, 
-            zoom='0-6',
+            zoom='0-6', # Specify zoom levels
             nb_processes=2,
-            profile='raster',
+            profile='raster', # Use raster profile
         )
         print("Tile generation completed successfully!")
     else:
